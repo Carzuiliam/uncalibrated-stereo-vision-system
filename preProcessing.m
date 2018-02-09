7ed97bc97f2cfce18c8806cfea76d6da249bdb46
Carzuilha <26937671+Carzuilha@users.noreply.github.com>
2018-02-09 00:28:47 -0200

    Added new pre-processing features.

--- preProcessing.m
+++ preProcessing.m
@@ -4,7 +4,7 @@
 %   This script is responsible for the pre-processing (for now, just an
 % greyscale conversion and resizing) for each stereo image.
 %
-%   (Maybe I'll try new concepts in the future...)
+%   (Maybe I'll try new concepts in the future, so stay alert!)
 %==========================================================================
 
 function [lSnap, rSnap] = preProcessing(lSnap, rSnap)
@@ -14,6 +14,14 @@ if size(lSnap, 3) == 3
     lSnap = rgb2gray(lSnap);
     rSnap = rgb2gray(rSnap);
 end
+
+%   Increases the image intensity (contrast) of each images.
+lSnap = imadjust(lSnap);
+rSnap = imadjust(rSnap);
+
+%   Increases the sharpness of each images.
+lSnap = imsharpen(lSnap);
+rSnap = imsharpen(rSnap);
 
 %   Resize the images.
 lSnap = imresize(lSnap, [240 320]);
